e34f2276a348c8d3ea45409699a16e6f8c6f999b
WillemB2104 <willembenjamin@gmail.com>
2015-12-04 15:18:58 +0100

    d3map final update

--- homework/week6/d3map/d3map.js
+++ homework/week6/d3map/d3map.js
@@ -10,67 +10,72 @@ var flattened_country_codes = country_codes.reduce(function(a, b) {
         return a.concat(b);
     });
 
+
+var dataPoint = function (x, y) {
+  this.date = x;
+  this.temperature = y;
+};
+
 d3.json("data.json", function(error, json) {
     if (error)
         return console.warn(error);
     data = json;
+    data.sort(function (a, b) {
+        num1 = parseInt(a.pop_dens);
+        num2 = parseInt(b.pop_dens);
+        if (num1 > num2)
+          return 1;
+        if (num1 < num2)
+          return -1;
+        return 0;
+    });
     datadict = {};
-    fillsdict = {
-         defaultFill: '#D3D3D3'
-    };
+    colorscheme = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026']
+    fillsdict = {defaultFill: '#D3D3D3'};
+    var i = 0, c = 0, bin = data.length / colorscheme.length;
+    fillkeys = ['<'+data[Math.floor(bin)].pop_dens]
+    for (x = Math.floor(bin); x < data.length - (2 * Math.floor(bin)); x += Math.floor(bin)) {
+        fillkeys.push(data[x].pop_dens + '-' + data[x + Math.floor(bin)].pop_dens)
+    }
+    fillkeys.push('>' + data[data.length - Math.floor(bin)].pop_dens)
+    fillkeys.forEach(function(key) {
+        fillsdict[key] = colorscheme[c];
+        c++;
+    });
     data.forEach(function(entry) {
         code_index = flattened_country_codes.indexOf(entry.name);
         country_code = flattened_country_codes[code_index - 1];
-        entry.fillKey = entry.name;
-        entry.area = parseInt(entry.area)
-        entry.pop_dens = parseInt(entry.pop_dens)
-        entry.total_pop = parseInt(entry.total_pop)
+        entry.fillKey = fillkeys[Math.floor(i/bin)];
+        entry.area = parseInt(entry.area);
+        entry.pop_dens = parseInt(entry.pop_dens);
+        entry.total_pop = parseInt(entry.total_pop);
         datadict[country_code] = entry;
+        i++;
     });
-    var max = Math.max.apply(null, Object.keys(datadict).map(function(e) { return datadict[e]['pop_dens'];}));
-    var min = Math.min.apply(null, Object.keys(datadict).map(function(e) { return datadict[e]['pop_dens'];}));
-    var root_max = Math.sqrt(max);
-    var root_min = Math.sqrt(min);
-    var range = root_max - root_min;
 
-    for (key in datadict) {
-        intensity = (Math.sqrt(datadict[key].pop_dens) - root_min)/ range;
-        color = returnColor(intensity);
-        fillsdict[datadict[key].fillKey] = color;
-    }
-    /* returnColor calculates RGB in white to red gradient based on given intensity (float between 0 - 1*/
-    function returnColor(intensity) {
-        r_rgb = "ff";
-        g_rgb = Math.ceil(220 - (220 * intensity)).toString(16);
-        if (g_rgb.length == 1){
-            g_rgb = "0" + g_rgb;
-        }
-        b_rgb = Math.ceil(220 - (220 * intensity)).toString(16);
-        if (b_rgb.length == 1){
-            b_rgb = "0" + b_rgb;
-        }
-        color = '#' + r_rgb + g_rgb + b_rgb;
-        return color;
-    }
-
-    myMap = $("#container1").datamap({
+    var myMap = new Datamap({
         scope: 'world',
         element: document.getElementById('container1'),
-        geography_config: {
+        projection: "mercator",
+        geographyConfig: {
             borderColor: 'rgba(255,255,255,0.3)',
+            highlightFillColor: 'rgba(63, 191, 63,1)',
             highlightBorderColor: 'rgba(0,0,0,0.5)',
-            popupTemplate: _.template([
-                '<div class="hoverinfo">',
-                '<% if (data.name) { %>',
-                '<strong><%= data.name %></strong><br/>',
-                'Area (km): <%= data.area %><br/>',
-                'Total population: <%= data.total_pop %><br/>',
-                'Population density: <%= data.pop_dens %><br/> <% } else { %>',
-                '<%= geography.properties.name %>  <% } %>',
-                '</div>'
-            ].join('') )
-        },
+            popupTemplate: function(geo, data) {
+                if (!data) {
+                    return ['<div class="hoverinfo">',
+                    '<strong>' + geo.properties.name + '</strong>: N/A',
+                    '</div>'].join('');
+                    }
+                return ['<div class="hoverinfo">',
+                '<strong>' + data.name + '</strong><br/>',
+                'Area (km): ' + data.area + '<br/>',
+                'Total population: ' + data.total_pop + '<br/>',
+                'Population density:' + data.pop_dens + '<br/>',
+                '</div>'].join('');
+                }
+            },
         fills: fillsdict,
-        data: datadict
+        data: datadict,
     });
 });
